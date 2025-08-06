9d0301aac305a2df895e6b666e28d8db3a7458f0
Alexander Theißen <alex.theissen@me.com>
2025-08-06 16:06:19 +0200

    Revert "pallet-revive benchmarks broken, use SubstrateWeights"
    This reverts commit 7522ff0cab4a14c833f98b6a89a9dbdaaca039d5.

--- system-parachains/asset-hubs/asset-hub-kusama/src/weights/mod.rs
+++ system-parachains/asset-hubs/asset-hub-kusama/src/weights/mod.rs
@@ -33,8 +33,7 @@ pub mod pallet_nft_fractionalization;
 pub mod pallet_nfts;
 pub mod pallet_proxy;
 pub mod pallet_remote_proxy;
-// TODO(#840): uncomment this so that pallet-revive is also benchmarked with this runtime
-// pub mod pallet_revive;
+pub mod pallet_revive;
 pub mod pallet_session;
 pub mod pallet_timestamp;
 pub mod pallet_transaction_payment;
